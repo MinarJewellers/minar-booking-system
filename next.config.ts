1a5bdb2198df29a674a0243abf34f25ede14ebad
MinarJewellers <ashish@minarjewellers.co.uk>
2026-05-18 17:11:39 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,3 +1,22 @@
 import type { NextConfig } from "next";
-const nextConfig: NextConfig = { reactStrictMode: true };
+
+const nextConfig: NextConfig = {
+  reactStrictMode: true,
+
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Content-Security-Policy",
+            value:
+              "frame-ancestors 'self' https://www.minarjewellers.com https://minarjewellers.com;",
+          },
+        ],
+      },
+    ];
+  },
+};
+
 export default nextConfig;
